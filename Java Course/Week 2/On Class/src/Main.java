6682f42c5fdf11ecb222b35582e4338ee1f02e72
LDCin <lechuyen48@gmail.com>
2025-06-26 21:45:23 +0700

    Delete Java Course/Week 2 directory

--- Java Course/Week 2/On Class/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String s = "abc";
-        String s_ = new String("abc");
-        System.out.println("s == s_: " + (s == s_));
-        System.out.println("s.equals(s_): " + s.equals(s_));
-    }
-}
